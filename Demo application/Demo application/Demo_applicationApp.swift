bc20bfd335e1eb1681a133489607d3f7e6e234d9
Tosif Bhatti <>
2023-01-11 10:56:03 +0530

    no message

---
+++ Demo application/Demo application/Demo_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_applicationApp.swift
+//  Demo application
+//
+//  Created by Tosif Bhatti on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo1/Demo1/Login.swift
@@ -0,0 +1,32 @@
+//
+//  Login.swift
+//  Demo1
+//
+//  Created by Tosif Bhatti on 09/01/23.
+//
+
+import UIKit
+
+class Login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func btnClick(_ sender: UIButton, forEvent event: UIEvent) {
+        print("Button Is Clicked")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
